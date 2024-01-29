b6a7d1a46ebb171618758aa49738a3d2028135a3
Tamilselvan1255 <tamilselvan0677@gmail.com>
2024-01-29 21:47:14 +0530

    updated

--- server/passport.js
+++ server/passport.js
@@ -1,4 +1,5 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
 const passport = require("passport");
 
 passport.use(
@@ -23,6 +24,29 @@ passport.use(
 	)
 );
 
+passport.use(
+	new FacebookStrategy(
+	  {
+		clientID: process.env.FACEBOOK_APP_ID,
+		clientSecret: process.env.FACEBOOK_APP_SECRET,
+		callbackURL: "http://localhost:8080/auth/facebook/callback",
+		scope: ["id", "displayName", "photos", "email"],
+	  },
+	  function (req, accessToken, refreshToken, profile, done) {
+		process.nextTick(function () {
+		  // Log user details to the console
+		  console.log("User Details:", profile);
+  
+		  // You can also log specific information, e.g., user email
+		  console.log("User Email:", profile.emails ? profile.emails[0].value : '');
+  
+		  // Continue with your existing callback logic
+		  return done(null, profile);
+		});
+	  }
+	)
+  );
+
 passport.serializeUser((user, done) => {
 	done(null, user);
 });
@@ -30,3 +54,38 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((user, done) => {
 	done(null, user);
 });
+
+// const GoogleStrategy = require("passport-google-oauth20").Strategy;
+// const passport = require("passport");
+
+// passport.use(
+// 	new GoogleStrategy(
+// 		{
+// 			clientID: process.env.CLIENT_ID,
+// 			clientSecret: process.env.CLIENT_SECRET,
+// 			callbackURL: "http://localhost:8080/auth/google/callback",
+// 			scope: ["profile", "email"],
+// 		},
+// 		function (accessToken, refreshToken, profile, callback) {
+// 			callback(null, profile);
+// 			// Log user details to the console
+// 			console.log("User Details:", profile);
+
+// 			// You can also log specific information, e.g., user email
+// 			console.log("User Email:", profile.emails[0].value);
+	  
+// 			// Continue with your existing callback logic
+// 			callback(null, profile);
+// 		}
+// 	)
+// );
+
+// passport.serializeUser((user, done) => {
+// 	done(null, user);
+// });
+
+// passport.deserializeUser((user, done) => {
+// 	done(null, user);
+// });
+
+
